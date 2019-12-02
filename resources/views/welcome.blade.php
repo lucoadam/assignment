45d42bf3981be9317ef8ccf0dd46964ccd24b91d
Lucoa Adams <lucoadam@gmail.com>
2019-12-03 02:14:57 +0545

    good added to homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,26 @@
         <div class="row">
             <div class="col-md-8 offset-md-2">
                 <h1 class="text-center">The Revolution is Here.</h1>
-                <div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item"></iframe></div>
+{{--                <div class="embed-responsive">--}}
+                    @php
+                        $goods = \App\Good::orderBy('created_at','desc')->get();
+                    @endphp
+                    @if(isset($goods))
+                        <div class="row">
+                        @foreach($goods as $good)
+
+                            <div class="col-md-4">
+                                <div class="card bg-dark">
+                                    <div class="card-body">
+                                        <img src="{{$good->image}}" class="embed-responsive">
+                                        <h4 class="navbar-text">{{$good->name}}</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            @endforeach
+                        </div>
+                        @endif
+                </div>
             </div>
             <div class="col"></div>
         </div>
